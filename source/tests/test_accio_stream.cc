ebbb2b05476f914b4e097582723f67a1d80dd7d2
Ete Remi <remi.ete@desy.de>
2018-09-27 13:53:47 +0200

    test_accio_stream now dumps all LCIO records from input file

--- source/tests/test_accio_stream.cc
+++ source/tests/test_accio_stream.cc
@@ -1,14 +1,6 @@
 
 #include <accio/stream.h>
 
-#define do_test(name, boolean) \
-  { \
-    if(not (boolean)) { \
-      std::cout << "Test: " << name << " failed!" <<  std::endl; \
-      return 1; \
-    } \
-  }
-
 int main(int argc, char **argv) {
   accio::stream fstream("TestStream");
   
@@ -16,20 +8,30 @@ int main(int argc, char **argv) {
     return 1;
   }
   const std::string fname = argv[1];
-  std::cout << "opening input file " << fname << std::endl;
-  do_test("opening input file", accio::error_codes::stream::success == fstream.open(fname, accio::io::open_mode::read));
-  accio::record_info rinfo;
-  std::cout << "read 1 rst record" << std::endl;
-  do_test("read 1 rst record", accio::error_codes::stream::success == fstream.read_next_record_info(rinfo));
-  std::cout << "== 1rst record in file ==" << std::endl;
-  std::cout << "   - option word: " << rinfo.m_options << std::endl;
-  std::cout << "   - len:         " << rinfo.m_length << std::endl;
-  std::cout << "   - clen:        " << rinfo.m_clength << std::endl;
-  std::cout << "   - name:        " << rinfo.m_name << std::endl;
+  std::cout << "Read records from file " << fname << std::endl;
+  if(accio::error_codes::stream::success != fstream.open(fname, accio::io::open_mode::read)) {
+    std::cout << "Couldn't open file '" << fname << "'" << std::endl;
+    return 1;
+  }
   
+  unsigned int rec_id = 0;
+  
+  while(1) {
+    accio::record_info rinfo;
+    auto status = fstream.skip_next_record(rinfo);
+    
+    if(accio::error_codes::stream::success != status) {
+      break;
+    }
+    
+    std::cout << "== record no " << rec_id << " ==" << std::endl;
+    std::cout << "   - option word: " << rinfo.m_options << std::endl;
+    std::cout << "   - len:         " << rinfo.m_length << std::endl;
+    std::cout << "   - clen:        " << rinfo.m_clength << std::endl;
+    std::cout << "   - name:        " << rinfo.m_name << std::endl;
+    
+    rec_id++;
+  }
 
-  // do_test("compare write and read", rval == wval);
-
-  std::cout << "TEST_PASSED" << std::endl;
   return 0;
 }
