7fbcb1a8408f5a07f2d3e33a0d9db497b6dd77eb
Ete Remi <remi.ete@desy.de>
2018-09-30 16:58:02 +0200

    Added pod tests for string classes, record header and block summary

--- source/tests/test_accio_pods.cc
+++ source/tests/test_accio_pods.cc
@@ -32,10 +32,21 @@ struct invalid_pod_1 {
 };
 
 int main() {
-
+  
   do_test("valid pod 1", std::is_pod<valid_pod_1>::value);
   do_test("valid pod 2", std::is_pod<valid_pod_2>::value);
   do_test("invalid pod 1", not std::is_pod<invalid_pod_1>::value);
+  
+  do_test("valid pod string 4", std::is_pod<accio::types::string4>::value);
+  do_test("valid pod string 8", std::is_pod<accio::types::string8>::value);
+  do_test("valid pod string 16", std::is_pod<accio::types::string16>::value);
+  do_test("valid pod string 32", std::is_pod<accio::types::string32>::value);
+  do_test("valid pod string 64", std::is_pod<accio::types::string64>::value);
+  do_test("valid pod string 128", std::is_pod<accio::types::string128>::value);
+  do_test("valid pod string 256", std::is_pod<accio::types::string256>::value);
+  
+  do_test("record header", std::is_pod<accio::io::record_header>::value);
+  do_test("block summary", std::is_pod<accio::io::block_summary>::value);
 
   std::cout << "TEST_PASSED" << std::endl;
   return 0;
