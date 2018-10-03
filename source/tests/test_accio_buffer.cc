68ff734b365a6ce85389930406c1fae4193cd42e
Remi Ete <remi.ete@gmail.com>
2018-10-03 17:58:05 +0200

    use new unit_test class for this test

--- source/tests/test_accio_buffer.cc
+++ source/tests/test_accio_buffer.cc
@@ -8,38 +8,35 @@
 // Author     : R.Ete
 //====================================================================
 
+// -- accio headers
+#include <accio/testing/unit_test.h>
 #include <accio/buffer.h>
 
-#define do_test(name, boolean) \
-  { \
-    if(not (boolean)) { \
-      std::cout << "Test: " << name << " failed!" <<  std::endl; \
-      return 1; \
-    } \
-  }
-
 int main() {
+
+  accio::unit_test test("accio_buffer_test");
+
   const accio::buffer<unsigned char>::size_type len = 1024*1024;
   accio::buffer<unsigned char> wbuf(len); // 1 Mo buffer, write mode
-  do_test("Buffer creation", wbuf.good());
+  test.test("Buffer creation", wbuf.good());
 
   wbuf.setstate(std::ios_base::eofbit);
-  do_test("eofbit", wbuf.eof());
-  do_test("not good", not wbuf.good());
-  do_test("not badbit", not wbuf.bad());
-  do_test("not failbit", not wbuf.fail());
+  test.test("eofbit", wbuf.eof());
+  test.test("not good", not wbuf.good());
+  test.test("not badbit", not wbuf.bad());
+  test.test("not failbit", not wbuf.fail());
   wbuf.clear_state();
 
-  do_test("buffer size", len == wbuf.size());
-  do_test("buffer tell start", 0 == wbuf.tell());
+  test.test("buffer size", len == wbuf.size());
+  test.test("buffer tell start", 0 == wbuf.tell());
   int wval = 12;
-  do_test("buffer write integer", sizeof(int) == wbuf.write_data(wval));
+  test.test("buffer write integer", sizeof(int) == wbuf.write_data(wval));
 
   // read mode, use the previous buffer
   accio::buffer<unsigned char> rbuf(wbuf.begin(), wbuf.tell(), true);
   int rval = 0;
-  do_test("buffer read back integer", sizeof(int) == rbuf.read_data(rval));
-  do_test("compare write and read", rval == wval);
+  test.test("buffer read back integer", sizeof(int) == rbuf.read_data(rval));
+  test.test("compare write and read", rval == wval);
 
   std::cout << "TEST_PASSED" << std::endl;
   return 0;
